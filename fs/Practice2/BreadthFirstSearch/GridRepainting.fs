a89b2ad7cc35cd82b29040d119d07ae93540dd49
dsudo <dsudo0624@gmail.com>
2020-10-30 12:09:09 +0900

    refactor

--- fs/Practice2/BreadthFirstSearch/GridRepainting.fs
+++ fs/Practice2/BreadthFirstSearch/GridRepainting.fs
@@ -1,19 +1,19 @@
 // https://atcoder.jp/contests/abc088/tasks/abc088_d
 
 (-1, -9)
-|> fun (WHITE, BLACK) ->
+|> fun (white, black) ->
   stdin.ReadLine()
   |> fun x -> x.Split()
   |> Array.map int
   |> fun x -> (x.[0], x.[1])
   |> fun (h, w) ->
-    Array.init h (fun _ -> stdin.ReadLine() |> Seq.map (fun x -> if x = '.' then WHITE else BLACK) |> Seq.toArray)
+    Array.init h (fun _ -> stdin.ReadLine() |> Seq.map (fun x -> if x = '.' then white else black) |> Seq.toArray)
     |> fun (grid: int [] []) ->
       (
-        Array.concat [ [| Array.create w BLACK |]; grid; [| Array.create w BLACK |] ]
-        |> Array.map (fun row -> Array.concat [ [| BLACK |]; row; [| BLACK |] ]),
+        Array.concat [ [| Array.create w black |]; grid; [| Array.create w black |] ]
+        |> Array.map (fun row -> Array.concat [ [| black |]; row; [| black |] ]),
         grid
-        |> Array.sumBy (Array.filter ((=) WHITE) >> Array.length)
+        |> Array.sumBy (Array.filter ((=) white) >> Array.length)
       )
     |> fun (grid: int [] [], wcount: int) ->
       (grid.[1].[1] <- 1)
@@ -32,14 +32,14 @@
                   (i + 1, j    );
                   (i    , j - 1);
                 ]
-                |> List.filter (fun (i2, j2) -> grid.[i2].[j2] = WHITE)
+                |> List.filter (fun (i2, j2) -> grid.[i2].[j2] = white)
                 |> List.map (fun (i2, j2) -> (grid.[i2].[j2] <- step + 1) |> fun () -> (i2, j2))
                 |> fun ts2 -> queue.[0] <- ts @ ts2
         )
-        |> Seq.takeWhile (fun _ -> grid.[h].[w] = WHITE && (queue.[0] |> Seq.isEmpty |> not))
+        |> Seq.takeWhile (fun _ -> grid.[h].[w] = white && (queue.[0] |> Seq.isEmpty |> not))
         |> Seq.length
         |> fun _ -> grid.[h].[w]
-        |> fun step -> if step = WHITE then step else wcount - step
+        |> fun step -> if step = white then step else wcount - step
 |> printfn "%d"
 
 // https://atcoder.jp/contests/abc088/submissions/17528933
