5273af44da58a94b43f0f655e9a66595705d820b
dsudo <dsudo0624@gmail.com>
2020-11-22 14:06:08 +0900

    fix LongestCommonSubsequence.fs

--- fs/Practice2/DynamicProgramming/LongestCommonSubsequence.fs
+++ fs/Practice2/DynamicProgramming/LongestCommonSubsequence.fs
@@ -5,44 +5,42 @@ stdin.ReadLine()
 |> fun q -> Array.init q (fun _ -> (stdin.ReadLine() |> Seq.toArray, stdin.ReadLine() |> Seq.toArray))
 |> Array.map (
   fun (xs : char [], ys : char []) ->
-    (
-      (Array.create (ys.Length + 1) 0),
-      (Array.create 123 0)
-    )
-    |> fun (memo : int [], xcmap : int []) ->
+    (Array.create (ys.Length + 1) 0)
+    |> fun (memo : int []) ->
       xs
       |> Array.map (
         fun x ->
-          (
-            xcmap.[(int x)],
-            [| 0 |]
-          )
-          |> fun (xc, yc) ->
+          [| false |]
+          |> fun (used : bool []) ->
             ys
             |> Array.mapi (
               fun i y ->
-                (
-                  if yc.[0] >= 0 && x = y
-                  then
-                    (
-                      if xc = yc.[0]
-                      then
-                        (
-                          (xcmap.[(int x)] <- xcmap.[(int x)] + 1),
-                          (yc.[0] <- -1)
-                        )
-                        |> fun _ -> 1
-                      else
-                        (yc.[0] <- yc.[0] + 1)
-                        |> fun _ -> 0
-                    )
-                  else 0
-                )
-                |> fun p ->
-                  memo.[i + 1] <- memo.[i + 1] |> max (memo.[i] + p) 
-            )
+                match y with
+                | _ when memo.[i] < memo.[i + 1] -> (used.[0] <- false)
+                | _ when not used.[0] && x = y   -> (used.[0] <- true) |> fun () -> memo.[i + 1] <- memo.[i] + 1
+                | _ when memo.[i] = memo.[i + 1] -> (used.[0] <- false)
+                | _ -> memo.[i + 1] <- memo.[i]
+          )
       )
       |> fun _ -> memo.[ys.Length]
 )
 |> Array.fold (fun acc x -> acc + (string x) + "\n") ""
 |> printfn "%s"
+
+
+// intput:
+// abcbdab
+// bdcababb
+
+// memo:
+// [|'b'; 'd'; 'c'; 'a'; 'b'; 'a'; 'b'; 'b'|]
+// 'a': [|0; 0; 0; 0; 1; 1; 1; 1; 1|]
+// 'b': [|0; 1; 1; 1; 1; 2; 2; 2; 2|]
+// 'c': [|0; 1; 1; 2; 2; 2; 2; 2; 2|]
+// 'b': [|0; 1; 1; 2; 2; 3; 3; 3; 3|]
+// 'd': [|0; 1; 2; 2; 2; 3; 3; 3; 3|]
+// 'a': [|0; 1; 2; 2; 3; 3; 4; 4; 4|]
+// 'b': [|0; 1; 2; 2; 3; 4; 4; 5; 5|]
+
+// output:
+// 5
