a8c0052ed92d2427cbe8c0b19807bc0aada8485d
dsudo <dsudo0624@gmail.com>
2021-01-29 21:55:21 +0900

    03_ShiftOnly.scala

---
+++ scala/BeginnersSelection/03_ShiftOnly.scala
@@ -0,0 +1,19 @@
+// https://atcoder.jp/contests/abs/tasks/abc081_b
+
+object Main {
+
+  implicit class Pipeline[T](x: T) {
+    def |>[S](f: T => S): S = f(x)
+  }
+
+  def main(args: Array[String]) = (
+    io.StdIn.readLine
+      |> (_ => io.StdIn.readLine)
+      |> (_.split(" ").map(_.toInt).reduce((x, a) => x | a))
+      |> (_.toBinaryString)
+      |> ((x) => x.length - s.lastIndexOf('1') - 1)
+      |> println
+  )
+}
+
+// https://atcoder.jp/contests/abs/submissions/19754776
