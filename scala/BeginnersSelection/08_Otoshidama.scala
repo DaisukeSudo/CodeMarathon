7a38957423fd730bb400286df156c91014a6fd94
dsudo <dsudo0624@gmail.com>
2021-01-30 21:52:00 +0900

    08_Otoshidama.scala

---
+++ scala/BeginnersSelection/08_Otoshidama.scala
@@ -0,0 +1,35 @@
+// https://atcoder.jp/contests/abs/tasks/abc085_c
+
+object Main {
+
+  implicit class Pipeline[T](x: T) {
+    def |>[S](f: T => S): S = f(x)
+  }
+
+  def main(args: Array[String]) = (
+    io.StdIn.readLine
+      .split(" ")
+      .map(_.toInt)
+      |> ((a) => (a(0), a(1) / 1000))
+      |> { case (n, y) => (
+        (0 to n)
+          .find((a) =>
+            (y - n - 9 * a) % 4 == 0 &&
+              (y - n - 9 * a) >= 0 &&
+              n - a - (y - n - 9 * a) / 4 >= 0
+          )
+          |> ((ao: Option[Int]) =>
+            ao match {
+              case Some(a) => (
+                ((y - n - 9 * a) / 4)
+                |> ((b: Int) => "%d %d %d".format(a, b, (n - a - b)))
+              )
+              case None => "-1 -1 -1"
+            }
+          )
+      )}
+      |> println
+  )
+}
+
+// https://atcoder.jp/contests/abs/submissions/19804500
