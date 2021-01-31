4b959d57e2815f4a7c857c49ef3fe90321345cd3
dsudo <dsudo0624@gmail.com>
2021-01-31 15:16:20 +0900

    10_Traveling.scala

---
+++ scala/BeginnersSelection/10_Traveling.scala
@@ -0,0 +1,43 @@
+// https://atcoder.jp/contests/abs/tasks/arc089_a
+
+object Main {
+
+  implicit class Pipeline[T](x: T) {
+    def |>[S](f: T => S): S = f(x)
+  }
+
+  def main(args: Array[String]) = (
+    io.StdIn.readLine.toInt
+    |> ((n) =>
+      (1 to n).map(_ => (
+        io.StdIn.readLine
+          .split(" ")
+          .map(_.toInt)
+          |> ((x) => (x(0), x(1), x(2)))
+      ))
+      .foldLeft(Option(0, 0, 0)) { case (acc, (t2, x2, y2)) =>
+        acc match {
+          case Some((t1, x1, y1)) => (
+            if (
+              ((t2 & 1) == ((x2 + y2) & 1))
+                && ((t2 - t1) >= (math.abs(x2 - x1) + math.abs(y2 - y1)))
+            )
+              Some (t2, x2, y2)
+            else
+              None
+          )
+          case None => None
+        }
+      }
+    )
+    |> ((x) =>
+      x match {
+        case Some(x) => "Yes"
+        case None    => "No"
+      }
+    )
+    |> println
+  )
+}
+
+// https://atcoder.jp/contests/abs/submissions/19835524
