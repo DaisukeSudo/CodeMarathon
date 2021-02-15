3f75b479452721a67d73cadd4509bf6a7497b8fd
dsudo <dsudo0624@gmail.com>
2021-02-15 23:49:11 +0900

    DarumaOtoshi_fixed.fsx 2

--- fs/Practice2/DynamicProgramming/Section/DarumaOtoshi_fixed.fsx
+++ fs/Practice2/DynamicProgramming/Section/DarumaOtoshi_fixed.fsx
@@ -15,22 +15,23 @@ Seq.initInfinite (fun _ -> stdin.ReadLine ())
 |> List.iter (fun (n, ws) ->
   (Array.init n (fun _ -> Array.create n 0))
   |> fun memo ->
-    [1..n]
-    |> List.iter (fun i ->
-      [for j in 0..(n - i - 1) -> (j, j + i)]
-      |> List.iter (fun (s, e) ->
-        if (memo.[s + 1].[e - 1] = i - 1)
-          && (ws.[s] - ws.[e] |> abs |> ((>=) 1))
-        then
-          memo.[s].[e] <- i + 1
-        else
-          [for k in s..(e - 1) -> ((s, k), (k + 1, e))]
-          |> List.iter (fun ((s1, e1), (s2, e2)) ->
-            (memo.[s1].[e1] + memo.[s2].[e2])
-            |> max memo.[s].[e]
-            |> fun x -> memo.[s].[e] <- x
-          )
-      )
+    seq {
+      for i in 1..n do
+      for j in 0..(n - i - 1) do
+      yield (i, j, j + i)
+    }
+    |> Seq.iter (fun (i, s, e) ->
+      if (ws.[s] - ws.[e] |> abs |> ((>=) 1))
+        && (memo.[s + 1].[e - 1] = i - 1)
+      then
+        memo.[s].[e] <- i + 1
+      else
+        seq { for k in s..(e - 1) -> ((s, k), (k + 1, e)) }
+        |> Seq.iter (fun ((s1, e1), (s2, e2)) ->
+          (memo.[s1].[e1] + memo.[s2].[e2])
+          |> max memo.[s].[e]
+          |> fun x -> memo.[s].[e] <- x
+        )
     )
   |> fun _ -> memo.[0].[n - 1]
   |> printfn "%d"
