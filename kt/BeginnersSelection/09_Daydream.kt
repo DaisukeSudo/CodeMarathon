e3ffba579bcba0159be9dd61da88f9b22ea0ffc8
dsudo <dsudo0624@gmail.com>
2021-03-18 08:52:17 +0900

    09_Daydream.kt

---
+++ kt/BeginnersSelection/09_Daydream.kt
@@ -0,0 +1,18 @@
+// https://atcoder.jp/contests/abs/tasks/arc065_a
+
+fun main(args: Array<String>) = (
+  readLine()!!
+    .let { s ->
+      listOf("eraser", "erase", "dreamer", "dream")
+        .fold(listOf(s)) { acc, word ->
+          acc.flatMap { x ->
+            x.split(word).filter { it != "" }
+          }
+        }
+        .size
+    }
+    .let { if (it == 0) "YES" else "NO" }
+    .let(::println)
+)
+
+// https://atcoder.jp/contests/abs/submissions/21009434
