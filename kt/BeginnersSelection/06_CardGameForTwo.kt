f517c4c023144c98767c0ce35b5ac4c121f9b088
dsudo <dsudo0624@gmail.com>
2021-03-18 23:06:12 +0900

    06_CardGameForTwo.kt updated

--- kt/BeginnersSelection/06_CardGameForTwo.kt
+++ kt/BeginnersSelection/06_CardGameForTwo.kt
@@ -2,24 +2,22 @@
 
 fun main(args: Array<String>) = (
   readLine()!!
-  pp { _x: Any ->
-    readLine()!!
-      .split(" ")
-      .map(String::toInt)
-      .sorted()
-      .reversed()
-      .withIndex()
-      .fold(0 to 0, { a, x ->
-        if (x.index % 2 == 0)
-          a.first + x.value to a.second
-        else
-          a.first to a.second + x.value
-      })
-  }
-  pp { (a, b) -> a - b }
-  pp ::println
+    .let { _ ->
+      readLine()!!
+        .split(" ")
+        .map(String::toInt)
+        .sorted()
+        .reversed()
+        .withIndex()
+        .fold(0 to 0, { a, x ->
+          if (x.index % 2 == 0)
+            a.first + x.value to a.second
+          else
+            a.first to a.second + x.value
+        })
+    }
+    .let { (a, b) -> a - b }
+    .let(::println)
 )
 
-infix fun <T, R> T.pp(f: (T) -> R): R = f(this)
-
-// https://atcoder.jp/contests/abs/submissions/21003740
+// https://atcoder.jp/contests/abs/submissions/21021940
