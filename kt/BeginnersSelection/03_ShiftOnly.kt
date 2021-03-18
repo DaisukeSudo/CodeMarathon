ddeed7522e62ee0c887a1c952ddefbfdaccbf954
dsudo <dsudo0624@gmail.com>
2021-03-18 22:57:43 +0900

    03_ShiftOnly.kt updated

--- kt/BeginnersSelection/03_ShiftOnly.kt
+++ kt/BeginnersSelection/03_ShiftOnly.kt
@@ -2,17 +2,15 @@
 
 fun main(args: Array<String>) = (
   readLine()!!
-  pp { _x: Any ->
-    readLine()!!
-      .split(" ")
-      .map(String::toInt)
-      .reduce { x, a -> x or a }
-      .toString(2)
-  }
-  pp { x -> x.length - x.lastIndexOf('1') - 1 }
-  pp ::println
+    .let { _ ->
+      readLine()!!
+        .split(" ")
+        .map(String::toInt)
+        .reduce { x, a -> x or a }
+        .toString(2)
+    }
+    .let { x -> x.length - x.lastIndexOf('1') - 1 }
+    .let(::println)
 )
 
-infix fun <T, R> T.pp(f: (T) -> R): R = f(this)
-
-// https://atcoder.jp/contests/abs/submissions/20984743
+// https://atcoder.jp/contests/abs/submissions/21021723
