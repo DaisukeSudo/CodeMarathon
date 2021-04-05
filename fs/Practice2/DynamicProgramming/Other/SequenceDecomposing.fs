8f9a4c5a802d144e41f856085f0ac6eb0a243b8a
dsudo <dsudo0624@gmail.com>
2021-04-06 08:33:32 +0900

    SequenceDecomposing.fs

---
+++ fs/Practice2/DynamicProgramming/Other/SequenceDecomposing.fs
@@ -0,0 +1,31 @@
+// https://atcoder.jp/contests/abc134/tasks/abc134_e
+
+stdin.ReadLine ()
+|> int
+|> fun n ->
+  (Array.create (n + 1) System.Int32.MaxValue)
+  |> fun memo ->
+    (List.init n (fun _ -> stdin.ReadLine () |> int))
+    |> List.fold (
+      fun len x ->
+        if memo.[len] >= x
+        then
+          (memo.[len + 1] <- x)
+          |> fun _ -> len + 1
+        else
+          [1..17]
+          |> List.fold (
+            fun (li, ri) i ->
+              (li + (ri - li) / 2)
+              |> fun mi ->
+                if memo.[mi] < x
+                then (li, mi)
+                else (mi + 1, ri)
+          ) (1, len)
+          |> fst
+          |> fun i -> memo.[i] <- x
+          |> fun _ -> len
+    ) 0
+|> printfn "%d"
+
+// https://atcoder.jp/contests/abc134/submissions/21518984
