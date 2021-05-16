eb094ce232e0951a2776ab3c7a34fb076fed69f5
dsudo <dsudo0624@gmail.com>
2021-05-16 17:19:03 +0900

    06_CardGameForTwo.jl updated

--- jl/BeginnersSelection/06_CardGameForTwo.jl
+++ jl/BeginnersSelection/06_CardGameForTwo.jl
@@ -5,15 +5,13 @@
   |> split
   |> a -> parse.(Int, a)
   |> a -> sort(a, rev = true)
-  |> a -> foldl((t, x) -> (
-    (a, b, i) -> (
-      i % 2 == 0
-        ? (a + x, b, i + 1)
-        : (a, b + x, i + 1)
-    )
-  )(t[1], t[2], t[3]), a, init = (0, 0, 0))
+  |> a -> foldl(((a, b, i), x) -> (
+    i % 2 == 0
+      ? (a + x, b, i + 1)
+      : (a, b + x, i + 1)
+  ), a, init = (0, 0, 0))
   |> x -> x[1] - x[2]
   |> println
 )
 
-# https://atcoder.jp/contests/abs/submissions/22578394
+# https://atcoder.jp/contests/abs/submissions/22656992
