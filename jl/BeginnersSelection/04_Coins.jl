1b471c49eae5a4aa8db5ebd73db71fc535d466bc
dsudo <dsudo0624@gmail.com>
2021-05-16 17:14:16 +0900

    04_Coins.jl updated

--- jl/BeginnersSelection/04_Coins.jl
+++ jl/BeginnersSelection/04_Coins.jl
@@ -3,20 +3,18 @@
 (
   (readline(), readline(), readline(), readline())
   |> a -> parse.(Int, a)
-  |> t -> (
-    (a, b, c, x) -> (
-      (0 : min(a, floor(x / 500)))
-      |> is -> map(i -> (
-        (0 : min(b, floor((x - 500 * i) / 100)))
-        |> js -> filter(j -> (
-          x - 500 * i - 100 * j <= 50 * c
-        ), js)
-        |> length
-      ), is)
-      |> sum
-    )
-  )(t[1], t[2], t[3], t[4])
+  |> ((a, b, c, x),) -> (
+    (0 : min(a, floor(x / 500)))
+    |> is -> map(i -> (
+      (0 : min(b, floor((x - 500 * i) / 100)))
+      |> js -> filter(j -> (
+        x - 500 * i - 100 * j <= 50 * c
+      ), js)
+      |> length
+    ), is)
+    |> sum
+  )
   |> println
 )
 
-# https://atcoder.jp/contests/abs/submissions/22560065
+# https://atcoder.jp/contests/abs/submissions/22656867
