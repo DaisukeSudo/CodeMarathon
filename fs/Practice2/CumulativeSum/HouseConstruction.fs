d33c4be78c066d22c2bebf39ea6dafdc876d6e30
dsudo <dsudo0624@gmail.com>
2021-11-10 22:56:44 +0900

    HouseConstruction.fs TLE 2

--- fs/Practice2/CumulativeSum/HouseConstruction.fs
+++ fs/Practice2/CumulativeSum/HouseConstruction.fs
@@ -9,27 +9,24 @@ stdin.ReadLine().Split() |> fun x -> (int x.[0], int x.[1], int64 x.[2], int64 x
   |> Array.concat
   |> fun (acc : int64[][]) ->
     (
-      seq { for i in 1..h do for j in 1..w -> (i, j) } |> Seq.iter (fun (i, j) ->
+      [1..h] |> List.iter (fun i -> [1..w] |> List.iter (fun j ->
         acc.[i].[j] <- acc.[i].[j] + acc.[i - 1].[j] + acc.[i].[j - 1] - acc.[i - 1].[j - 1]
-      ),
-      (fun (s1, e1, s2, e2) -> (s2 - s1) * (e2 - e1)),
+      )),
       (fun (s1, e1, s2, e2) -> acc.[s1].[e1] + acc.[s2].[e2] - acc.[s1].[e2] - acc.[s2].[e1]),
+      (fun (s1, e1, s2, e2) -> (s2 - s1) * (e2 - e1)),
       [| 0 |]
     )
-    |> fun (_, area, landCost, accArea) ->
-      seq {
-        for s1 in 0..h - 1 do
-        for e1 in 0..w - 1 do
-        for s2 in h..(-1)..s1 + 1 do
-        for e2 in w..(-1)..e1 + 1 -> (s1, e1, s2, e2)
-      }
-      |> Seq.iter (fun (s1, e1, s2, e2) ->
-        area (s1, e1, s2, e2)
-        |> fun curArea ->
-          if curArea > accArea.[0] && int64 curArea * k + landCost (s1, e1, s2, e2) <= v
-          then accArea.[0] <- curArea
-      )
+    |> fun (_, cost, area, accArea) ->
+      [0..h - 1] |> List.iter (fun s1 -> [0..w - 1] |> List.iter (fun e1 ->
+        [h..(-1)..s1 + 1] |> List.iter (fun s2 -> [w..(-1)..e1 + 1] |> List.iter (fun e2 ->
+          area (s1, e1, s2, e2)
+          |> fun curArea ->
+            if curArea > accArea.[0] && int64 curArea * k + cost (s1, e1, s2, e2) <= v
+            then accArea.[0] <- curArea
+        ))
+      ))
       |> fun () -> accArea.[0]
 |> printfn "%d"
 
 // https://atcoder.jp/contests/gigacode-2019/submissions/27166261
+// https://atcoder.jp/contests/gigacode-2019/submissions/27167608
