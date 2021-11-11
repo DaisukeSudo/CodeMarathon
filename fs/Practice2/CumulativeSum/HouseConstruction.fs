b5db5f175457436e181e578544df5fc0b306f8a0
dsudo <dsudo0624@gmail.com>
2021-11-11 12:06:42 +0900

    HouseConstruction.fs WA

--- fs/Practice2/CumulativeSum/HouseConstruction.fs
+++ fs/Practice2/CumulativeSum/HouseConstruction.fs
@@ -12,21 +12,33 @@ stdin.ReadLine().Split() |> fun x -> (int x.[0], int x.[1], int64 x.[2], int64 x
       [1..h] |> List.iter (fun i -> [1..w] |> List.iter (fun j ->
         acc.[i].[j] <- acc.[i].[j] + acc.[i - 1].[j] + acc.[i].[j - 1] - acc.[i - 1].[j - 1]
       )),
-      (fun (s1, e1, s2, e2) -> acc.[s1].[e1] + acc.[s2].[e2] - acc.[s1].[e2] - acc.[s2].[e1]),
-      (fun (s1, e1, s2, e2) -> (s2 - s1) * (e2 - e1)),
+      (fun (x1, y1, x2, y2) -> acc.[x1].[y1] + acc.[x2].[y2] - acc.[x1].[y2] - acc.[x2].[y1]),
+      (fun (x1, y1, x2, y2) -> (x2 - x1) * (y2 - y1)),
+      (fun x -> System.Math.Log(double x, 2.) |> ceil |> int),
       [| 0 |]
     )
-    |> fun (_, cost, area, accArea) ->
-      [0..h - 1] |> List.iter (fun s1 -> [0..w - 1] |> List.iter (fun e1 ->
-        [h..(-1)..s1 + 1] |> List.iter (fun s2 -> [w..(-1)..e1 + 1] |> List.iter (fun e2 ->
-          area (s1, e1, s2, e2)
-          |> fun curArea ->
-            if curArea > accArea.[0] && int64 curArea * k + cost (s1, e1, s2, e2) <= v
-            then accArea.[0] <- curArea
-        ))
-      ))
-      |> fun () -> accArea.[0]
+    |> fun (_, cost, area, limit, accArea) ->
+      [1..limit h + 1] |> List.fold (fun (lx1, rx1, _) _ ->
+        lx1 + (rx1 - lx1) / 2 |> fun x1 ->
+          [1..limit w + 1] |> List.fold (fun (ly1, ry1, _) _ ->
+            ly1 + (ry1 - ly1) / 2 |> fun y1 ->
+              [1..limit (h - x1) + 1] |> List.fold (fun (lx2, rx2, _) _ ->
+                lx2 + (rx2 - lx2) / 2 |> fun x2 ->
+                  [1..limit (w - y1) + 1] |> List.fold (fun (ly2, ry2, a) _ ->
+                    ly2 + (ry2 - ly2) / 2 |> fun y2 ->
+                      area (x1, y1, x2, y2) |> fun curArea ->
+                        if int64 curArea * k + cost (x1, y1, x2, y2) <= v
+                        then (accArea.[0] <- max accArea.[0] curArea) |> fun () -> (y2, ry2, true)
+                        else (ly2, y2 - 1, a)
+                  ) (y1 + 1, w + 1, false)
+                  |> fun (_, _, a) -> if a then (x2, rx2, a) else (lx2, x2 - 1, a)
+              ) (x1 + 1, h + 1, false)
+              |> fun (_, _, a) -> if a then (ly1, y1, a) else (y1 + 1, ry1, a)
+          ) (0, w, false)
+          |> fun (_, _, a) -> if a then (lx1, x1, a) else (x1 + 1, rx1, a)
+      ) (0, h, false)
+      |> fun _ -> accArea.[0]
 |> printfn "%d"
 
-// https://atcoder.jp/contests/gigacode-2019/submissions/27166261
 // https://atcoder.jp/contests/gigacode-2019/submissions/27167608
+// https://atcoder.jp/contests/gigacode-2019/submissions/27170972
