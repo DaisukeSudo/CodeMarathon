27d99b25344a6f006bed50ba03f04291f514dd30
dsudo <dsudo0624@gmail.com>
2021-11-16 07:28:55 +0900

    HouseConstruction.fs WA 2

--- fs/Practice2/CumulativeSum/HouseConstruction.fs
+++ fs/Practice2/CumulativeSum/HouseConstruction.fs
@@ -18,27 +18,27 @@ stdin.ReadLine().Split() |> fun x -> (int x.[0], int x.[1], int64 x.[2], int64 x
       [| 0 |]
     )
     |> fun (_, cost, area, limit, accArea) ->
-      [1..limit h + 1] |> List.fold (fun (lx1, rx1, _) _ ->
+      [0..limit h] |> List.fold (fun (lx1, rx1, ax1) _ ->
         lx1 + (rx1 - lx1) / 2 |> fun x1 ->
-          [1..limit w + 1] |> List.fold (fun (ly1, ry1, _) _ ->
+          [0..limit w] |> List.fold (fun (ly1, ry1, ay1) _ ->
             ly1 + (ry1 - ly1) / 2 |> fun y1 ->
-              [1..limit (h - x1) + 1] |> List.fold (fun (lx2, rx2, _) _ ->
+              [0..limit (h - x1)] |> List.fold (fun (lx2, rx2, ax2) _ ->
                 lx2 + (rx2 - lx2) / 2 |> fun x2 ->
-                  [1..limit (w - y1) + 1] |> List.fold (fun (ly2, ry2, a) _ ->
+                  [0..limit (w - y1)] |> List.fold (fun (ly2, ry2, ay2) _ ->
                     ly2 + (ry2 - ly2) / 2 |> fun y2 ->
                       area (x1, y1, x2, y2) |> fun curArea ->
                         if int64 curArea * k + cost (x1, y1, x2, y2) <= v
                         then (accArea.[0] <- max accArea.[0] curArea) |> fun () -> (y2, ry2, true)
-                        else (ly2, y2 - 1, a)
-                  ) (y1 + 1, w + 1, false)
-                  |> fun (_, _, a) -> if a then (x2, rx2, a) else (lx2, x2 - 1, a)
-              ) (x1 + 1, h + 1, false)
-              |> fun (_, _, a) -> if a then (ly1, y1, a) else (y1 + 1, ry1, a)
+                        else (ly2, y2, ay2)
+                  ) (y1, w + 1, false)
+                  |> fun (_, _, a) -> if a then (x2, rx2, true) else (lx2, x2, ax2)
+              ) (x1, h + 1, false)
+              |> fun (_, _, a) -> if a then (ly1, y1, true) else (y1, ry1, ay1)
           ) (0, w, false)
-          |> fun (_, _, a) -> if a then (lx1, x1, a) else (x1 + 1, rx1, a)
+          |> fun (_, _, a) -> if a then (lx1, x1, true) else (x1, rx1, ax1)
       ) (0, h, false)
       |> fun _ -> accArea.[0]
 |> printfn "%d"
 
 // https://atcoder.jp/contests/gigacode-2019/submissions/27167608
-// https://atcoder.jp/contests/gigacode-2019/submissions/27170972
+// https://atcoder.jp/contests/gigacode-2019/submissions/27174387
