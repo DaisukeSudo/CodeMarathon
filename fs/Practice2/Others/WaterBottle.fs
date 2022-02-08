c507fb1a9e1475ffe2d2d45bc3c9bf798cac4d80
dsudo <dsudo0624@gmail.com>
2022-02-08 22:10:15 +0900

    WaterBottle.fs AC

--- fs/Practice2/Others/WaterBottle.fs
+++ fs/Practice2/Others/WaterBottle.fs
@@ -2,12 +2,11 @@
 
 stdin.ReadLine().Split() |> Array.map float |> fun x -> (x.[0], x.[1], x.[2])
 |> fun (a, b, x) ->
-  [
-    (2. * b / a) - (2. * x / (a * a * a));
-    (a * b * b / (2. * x));
-  ]
-|> List.map (atan >> ((*) (180. / System.Math.PI)))
-|> List.max
+  if (x / a) > (a * b / 2.)
+  then (2. * b / a) - (2. * x / (a * a * a))
+  else (a * b * b / (2. * x))
+|> atan
+|> (*) (180. / System.Math.PI)
 |> printfn "%f"
 
-// https://atcoder.jp/contests/abc144/submissions/29166279
+// https://atcoder.jp/contests/abc144/submissions/29166488
