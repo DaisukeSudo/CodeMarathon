63cd93239d5edaf6787c549fa137419e13ef2e82
dsudo <dsudo0624@gmail.com>
2022-02-17 13:15:32 +0900

    StoneDroppingGame.fs AC

--- fs/Practice2/Others/StoneDroppingGame.fs
+++ fs/Practice2/Others/StoneDroppingGame.fs
@@ -8,14 +8,27 @@ stdin.ReadLine().Split() |> Array.map int |> fun x -> (x.[0], x.[1], x.[2])
     seq { for x in 1..h - 1 do for y in 0..w - 1 -> (x, y) } |> Seq.map (fun (x, y) ->
       newState ()
       |> fun state -> (state.[x].[y] <- None) |> fun () -> ([| true |], [| 0 |]) |> fun (continued, score) ->
-        Seq.initInfinite id
+        seq { 0..h - 1 }
         |> Seq.takeWhile (fun _ -> continued.[0])
         |> Seq.iter (fun i ->
           [
             continued.[0] <- false;
             (* dropping *)
             seq { for x in h - 1..(-1)..1 do for y in w - 1..(-1)..0 -> (x, y) } |> Seq.iter (fun (x, y) ->
-              if state.[x].[y] = None then (state.[x].[y] <- state.[x - 1].[y]) |> fun () -> (state.[x - 1].[y] <- None)
+              if state.[x].[y] = None then (
+                [| None |]
+                |> fun above ->
+                  seq { x - 1..(-1)..0 }
+                  |> Seq.takeWhile (fun _ -> above.[0] = None)
+                  |> Seq.iter (fun x2 ->
+                    if state.[x2].[y] <> None
+                    then (
+                      (above.[0] <- state.[x2].[y])
+                      |> fun () -> state.[x2].[y] <- None
+                    )
+                  )
+                  |> fun () -> state.[x].[y] <- above.[0]
+              )
             );
             (* erasing *)
             [1..h - 1] |> Seq.iter (fun x ->
@@ -32,9 +45,7 @@ stdin.ReadLine().Split() |> Array.map int |> fun x -> (x.[0], x.[1], x.[2])
                         prev |> Option.get |> ( * ) chain
                       )
                       |> fun (a, b) -> score.[0] <- score.[0] + (a * b)
-                      |> fun () -> ([1..chain] |> Seq.iter (fun y2 ->
-                        state.[x].[y - y2] <- None
-                      ))
+                      |> fun () -> [y - 1..(-1)..y - chain] |> Seq.iter (fun y2 -> state.[x].[y2] <- None)
                       |> fun () -> (continued.[0] <- true)
                     )
                     |> fun () -> (cur, 1)
@@ -49,3 +60,5 @@ stdin.ReadLine().Split() |> Array.map int |> fun x -> (x.[0], x.[1], x.[2])
     )
     |> Seq.max
 |> printfn "%d"
+
+// https://atcoder.jp/contests/s8pc-3/submissions/29381891
