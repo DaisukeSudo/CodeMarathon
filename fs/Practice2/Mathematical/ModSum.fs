b164fcec92b46a087a30a56ecd343b313a046287
dsudo <dsudo0624@gmail.com>
2022-02-27 17:55:47 +0900

    ModSum.fs

---
+++ fs/Practice2/Mathematical/ModSum.fs
@@ -0,0 +1,8 @@
+// https://atcoder.jp/contests/abc139/tasks/abc139_d
+
+stdin.ReadLine() |> int64
+|> fun n ->
+  n * (n - 1L) / 2L
+|> printfn "%d"
+
+// https://atcoder.jp/contests/abc139/submissions/29736087
