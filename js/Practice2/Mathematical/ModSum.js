c1f52cafa7d7b46b4f66470b6dba234e1736eb87
dsudo <dsudo0624@gmail.com>
2022-02-27 18:02:44 +0900

    ModSum.js WA

---
+++ js/Practice2/Mathematical/ModSum.js
@@ -0,0 +1,8 @@
+// https://atcoder.jp/contests/abc139/tasks/abc139_d
+
+const main = n =>
+  n * (n - 1) / 2;
+
+console.log(main(+require('fs').readFileSync(0).toString()));
+
+// https://atcoder.jp/contests/abc139/submissions/29736240
