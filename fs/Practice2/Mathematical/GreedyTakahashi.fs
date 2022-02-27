0718dad8e1ea0e9da6934f2517c4ee31762137eb
dsudo <dsudo0624@gmail.com>
2022-02-27 16:45:53 +0900

    GreedyTakahashi.fs 2

--- fs/Practice2/Mathematical/GreedyTakahashi.fs
+++ fs/Practice2/Mathematical/GreedyTakahashi.fs
@@ -2,9 +2,8 @@
 
 stdin.ReadLine().Split() |> Array.map int64 |> fun x -> (x.[0], x.[1], x.[2])
 |> fun (a, b, k) ->
-  [max 0L (a - k); max 0L (b - (max 0L (k - a)))]
-|> Seq.map string
-|> String.concat " "
-|> printfn "%s"
+  (max 0L (a - k), max 0L (b - (max 0L (k - a))))
+|> fun (a, b) ->
+  printfn "%d %d" a b
 
-// https://atcoder.jp/contests/abc149/submissions/29731802
+// https://atcoder.jp/contests/abc149/submissions/29734226
