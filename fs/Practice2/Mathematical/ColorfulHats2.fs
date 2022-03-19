0b615c698e9e65bdcb7536864a16a389fc2d6fe6
dsudo <dsudo0624@gmail.com>
2022-03-19 14:15:47 +0900

    ColorfulHats2.fs AC

--- fs/Practice2/Mathematical/ColorfulHats2.fs
+++ fs/Practice2/Mathematical/ColorfulHats2.fs
@@ -3,22 +3,19 @@
 stdin.ReadLine()
 |> fun _ -> stdin.ReadLine().Split() |> Array.map int
 |> Array.fold (fun (r, abc) x ->
-  (
-    (
-      abc
-      |> Seq.filter ((=) x)
-      |> Seq.length
-      |> int64
-      |> fun c -> r * c % 1_000_000_007L
-    ),
-    (
-      abc
-      |> Seq.findIndex ((=) x)
-      |> fun i -> Array.concat [ abc.[0..i - 1]; [| abc.[i] + 1 |]; abc.[i + 1..] ]
-    )
-  )
+  abc
+  |> Seq.filter ((=) x)
+  |> Seq.length
+  |> int64
+  |> fun c -> 
+    abc
+    |> Seq.tryFindIndex ((=) x)
+    |> fun i ->
+      match i with
+      | Some i -> (r * c % 1_000_000_007L, Array.concat [ abc.[0..i - 1]; [| abc.[i] + 1 |]; abc.[i + 1..] ])
+      | None -> (0L, [||])
 ) (1L, Array.create 3 0)
 |> fst
 |> printfn "%d"
 
-// https://atcoder.jp/contests/sumitrust2019/submissions/30218822
+// https://atcoder.jp/contests/sumitrust2019/submissions/30219032
