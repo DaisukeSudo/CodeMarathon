147e290bee1fb2ffbcc4def2d5d599cf64adf403
dsudo <dsudo0624@gmail.com>
2022-03-24 03:07:41 +0900

    DigitSumReplace.fs AC

--- fs/Practice2/Mathematical/DigitSumReplace.fs
+++ fs/Practice2/Mathematical/DigitSumReplace.fs
@@ -2,18 +2,8 @@
 
 stdin.ReadLine() |> int |> fun m -> seq {1..m}
 |> Seq.map (fun _ -> stdin.ReadLine().Split() |> (fun x -> int64 x.[0], int64 x.[1]))
-|> Seq.fold (fun (total, prev) (d, c) ->
-  (
-    c - 1L + c * d / 10L,
-    d * c % 9L
-  )
-  |> fun (subtotal, curr) ->
-    (
-      total + subtotal + (prev + curr + 10L) / 10L,
-      prev + curr % 9L
-    )
-) (-1L, 0L)
-|> fst
+|> Seq.fold (fun (digit, total) (d, c) -> (digit + c, total + d * c)) (-1L, -1L)
+|> fun (digit, total) -> digit + total / 9L
 |> printfn "%d"
 
-// https://atcoder.jp/contests/ddcc2020-qual/submissions/30369156
+// https://atcoder.jp/contests/ddcc2020-qual/submissions/30373665
