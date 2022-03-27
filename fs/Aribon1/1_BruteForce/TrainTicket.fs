05197c64be88aaa712b6524cfb28390124a4db01
dsudo <dsudo0624@gmail.com>
2022-03-27 17:00:10 +0900

    TrainTicket.fs refactor

--- fs/Aribon1/1_BruteForce/TrainTicket.fs
+++ fs/Aribon1/1_BruteForce/TrainTicket.fs
@@ -1,14 +1,14 @@
 // https://atcoder.jp/contests/abc079/tasks/abc079_c
 
 stdin.ReadLine() |> fun s ->
-  Array.create (1 <<< 3) (s.[0..0], int s.[0..0]) |> fun arr ->
-    [1..3] |> List.map (fun d -> [0..(1 <<< 3) - 1] |> List.iter (fun i ->
-      arr.[i] |> fun (w, v) ->
+  seq {0..(1 <<< s.Length - 1) - 1} |> Seq.map (fun i ->
+    [1..s.Length - 1] |> List.fold (fun (w, v) d ->
       if (i >>> d - 1) &&& 1 = 1
-      then arr.[i] <- (w + "+" + s.[d..d], v + int s.[d..d])
-      else arr.[i] <- (w + "-" + s.[d..d], v - int s.[d..d])
-    ))
-    |> fun _ -> arr |> Array.find (fun (_, v) -> v = 7) |> fun (w, _) -> w + "=7"
+      then (w + "+" + s.[d..d], v + int s.[d..d])
+      else (w + "-" + s.[d..d], v - int s.[d..d])
+    ) (s.[0..0], int s.[0..0])
+  )
+  |> Seq.find (fun (_, v) -> v = 7) |> fun (w, _) -> w + "=7"
 |> printfn "%s"
 
-// https://atcoder.jp/contests/abc079/submissions/30505116
+// https://atcoder.jp/contests/abc079/submissions/30505436
