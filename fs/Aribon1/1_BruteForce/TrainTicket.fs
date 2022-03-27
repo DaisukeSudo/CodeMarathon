4efa3b1fef9bc1a0b2dbd5dfad0d2889464f1952
dsudo <dsudo0624@gmail.com>
2022-03-27 17:15:08 +0900

    TrainTicket.fs refactor 2

--- fs/Aribon1/1_BruteForce/TrainTicket.fs
+++ fs/Aribon1/1_BruteForce/TrainTicket.fs
@@ -1,14 +1,14 @@
 // https://atcoder.jp/contests/abc079/tasks/abc079_c
 
-stdin.ReadLine() |> fun s ->
+stdin.ReadLine() |> Seq.map string |> Seq.toArray |> fun s ->
   seq {0..(1 <<< s.Length - 1) - 1} |> Seq.map (fun i ->
-    [1..s.Length - 1] |> List.fold (fun (w, v) d ->
+    [1..s.Length - 1] |> Seq.fold (fun (w, v) d ->
       if (i >>> d - 1) &&& 1 = 1
-      then (w + "+" + s.[d..d], v + int s.[d..d])
-      else (w + "-" + s.[d..d], v - int s.[d..d])
-    ) (s.[0..0], int s.[0..0])
+      then (w + "+" + s.[d], v + int s.[d])
+      else (w + "-" + s.[d], v - int s.[d])
+    ) (s.[0], int s.[0])
   )
   |> Seq.find (fun (_, v) -> v = 7) |> fun (w, _) -> w + "=7"
 |> printfn "%s"
 
-// https://atcoder.jp/contests/abc079/submissions/30505436
+// https://atcoder.jp/contests/abc079/submissions/30505879
