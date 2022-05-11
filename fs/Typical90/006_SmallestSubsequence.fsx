cc80014ca40b1efd144105450e2f60277f4d6c93
dsudo <dsudo0624@gmail.com>
2022-05-11 21:47:03 +0900

    006_SmallestSubsequence.fsx AC

--- fs/Typical90/006_SmallestSubsequence.fsx
+++ fs/Typical90/006_SmallestSubsequence.fsx
@@ -1,31 +1,24 @@
 // https://atcoder.jp/contests/typical90/tasks/typical90_f
 
-let n, k = stdin.ReadLine().Split() |> Array.map int |> fun x -> x.[0], x.[1]
-let s    = stdin.ReadLine()
+let k = stdin.ReadLine().Split() |> fun x -> int x.[1]
+let s = stdin.ReadLine()
 
-let cs = Array.zeroCreate 26 in s |> Seq.iter (int >> ((+) -97) >> fun x -> cs.[x] <- cs.[x] + 1)
-
-let nextCur =
-  fun cur ->
-    let mutable i = cur
-    let mutable next = true
-    while next do
-      if cs.[i] > 0 then
-        cs.[i] <- cs.[i] - 1
-        next <- false
-      else
-        i <- i - 1
-    i
-
-let reduce =
-  fun (s: string) (c: char) ->
-    let i = s |> Seq.findIndex ((=) c)
-    s.[0..i - 1] + s.[i + 1..]
-
-let res =
-  ((s, 25), [1..n - k]) ||> List.fold (fun (acc, cur) _ ->
-    let i = nextCur cur in reduce acc (i + 97 |> char), i
+((0, []), [0..k - 1]) ||> List.fold (fun (ofs, acc) i ->
+  let mutable isContinued = true
+  let mutable mi = -1
+  let mutable mc = '{'
+  seq {ofs..s.Length - k + i}
+  |> Seq.takeWhile (fun _ -> isContinued)
+  |> Seq.iter (fun j ->
+    if s.[j] < mc then mi <- j; mc <- s.[j]
+    if s.[j] = 'a' then isContinued <- false
   )
-  |> fst
+  (mi + 1, mc :: acc)
+)
+|> snd
+|> List.rev
+|> List.map string
+|> String.concat ""
+|> printfn "%s"
 
-printfn "%s" res
+// https://atcoder.jp/contests/typical90/submissions/31605691
