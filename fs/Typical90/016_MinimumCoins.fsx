b3faee6f17632ddc802b48853019754325abdb80
dsudo <dsudo0624@gmail.com>
2022-07-16 18:43:22 +0900

    016_MinimumCoins.fsx AC

--- fs/Typical90/016_MinimumCoins.fsx
+++ fs/Typical90/016_MinimumCoins.fsx
@@ -3,16 +3,14 @@
 let n = stdin.ReadLine() |> int
 let a, b, c = stdin.ReadLine().Split() |> Array.map int |> Array.sortDescending |> fun x -> x.[0], x.[1], x.[2]
 
-seq {0..n / a} |> Seq.collect (fun i ->
-  let n1 = n - a * i
-  seq {0..n1 / b} |> Seq.map (fun j ->
-    let n2 = n1 - b * j
-    if n2 % c = 0 then Some(i + j + n2 / c) else None
-  )
-)
-|> Seq.filter (Option.isSome)
-|> Seq.map (Option.get)
+seq {
+  for i in 0 .. (min (n / a) 9999) do
+    let n1 = n - a * i
+    for j in 0 .. (min (n1 / b) (9999 - i)) do
+      let n2 = n1 - b * j
+      if n2 % c = 0 then yield i + j + n2 / c
+}
 |> Seq.min
 |> stdout.WriteLine
 
-// https://atcoder.jp/contests/typical90/submissions/33254850
+// https://atcoder.jp/contests/typical90/submissions/33255332
