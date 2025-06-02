0ea111750673d6b94a04247c8d86bab5e32e4ffc
dsudo <24204273+DaisukeSudo@users.noreply.github.com>
2025-06-02 20:02:37 +0900

    FreedomOfExpression.fsx

---
+++ fs/Aribon1/3_DynamicPrograming/FreedomOfExpression.fsx
@@ -0,0 +1,52 @@
+// https://atcoder.jp/contests/arc004/tasks/arc004_4
+
+let factorize n =
+  let mutable x = n
+  let mutable ans = []
+  let z = int64 (sqrt (float n)) + 1L
+  for i in 2L :: [3L .. 2L .. z] do
+    let rec loop () =
+      if x % i = 0L then
+        x <- x / i
+        ans <- i :: ans
+        loop ()
+    loop ()
+  if x <> 1L then ans <- x :: ans
+  ans
+
+let p = 1_000_000_007L
+
+let power =
+  let rec loop m n =
+    match n with
+    | _ when n = 0L -> 1L
+    | _ when n % 2L = 0L -> loop m (n / 2L) |> (fun x -> x * x % p)
+    | _ -> m * loop m (n - 1L) % p
+  loop
+
+let inverse b = power b (p - 2L)
+
+let modDiv a b = a * (inverse b) % p
+
+let permutation n r = seq { 0L .. (r - 1L) } |> Seq.fold (fun acc i -> acc * (n - i) % p) 1L
+
+let combination n r = (permutation n r, permutation r r) ||> modDiv
+
+// ----
+
+let solve n m =
+  n
+  |> abs
+  |> factorize
+  |> List.countBy id
+  |> List.map (snd >> int64)
+  |> List.fold (fun acc e ->
+    (combination (e + m - 1L) (m - 1L)) * acc % p
+  ) 1L
+  |> (fun x -> x * power 2L (m - 1L) % p)
+
+stdin.ReadLine().Split() |> Array.map int64 |> fun x -> x.[0], x.[1]
+||> solve
+|> stdout.WriteLine
+
+// https://atcoder.jp/contests/arc004/submissions/66405008
